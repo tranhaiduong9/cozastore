2a5b04e29c3f54e5d1255586c00244b92b3a6e8f
tranhaiduong9 <duongth210897@gmail.com>
2024-02-27 22:11:24 +0700

    sign in sign up function

--- src/main/java/com/member/cozastore/security/SecurityConfig.java
+++ src/main/java/com/member/cozastore/security/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -45,6 +48,15 @@ public class SecurityConfig {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
